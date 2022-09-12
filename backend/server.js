3ffee3318661f97c8d5c342b16c55448b6d72064
lizcastroprogrammer <37345064+lizcastroprogrammer@users.noreply.github.com>
2022-09-11 21:42:17 -0500

    thank you brad traversy!

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const { connect } = require("mongoose");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -13,8 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/", require("./routes/home"));
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
